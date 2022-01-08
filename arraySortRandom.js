5687f956f145096ff3f9f986257ff64bcc42b9d2
Tina <67583197+codephoria@users.noreply.github.com>
2022-01-08 01:50:55 +0100

    Update arraySortRandom.js

--- arraySortRandom.js
+++ arraySortRandom.js
@@ -1,5 +1,33 @@
 const myArray = ["word1", "word2", "word3"];
 
+//basic
 myArray.sort(function(){
     return .5 - Math.random();
 });
+
+//Fisher-Yates
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+//Durstenfeld
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
